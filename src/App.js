0a9a1d1dcf1a197909543ca3d810e3881d9bd437
malmosands <matthias@c1x.dev>
2021-05-12 00:44:51 +0200

    basic react-spring fade in

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import logo from './logo.svg';
+import { useSpring, animated } from 'react-spring'
 import './App.css';
 
-function App() {
+function App() {  
+  const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <animated.div style={props}>I will fade in</animated.div>
       </header>
     </div>
   );
